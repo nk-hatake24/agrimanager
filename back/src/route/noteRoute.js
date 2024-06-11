6f11b9dce53d4a368361afb21a1e9ff6096458cd
Nkenla Michel (mitcode) <nkenlamichel@gmail.com>
2024-06-11 10:42:13 +0200

    conterisation

--- back/src/route/noteRoute.js
+++ back/src/route/noteRoute.js
@@ -2,9 +2,9 @@ const route = require('express').Router()
 const { authenticateJWT, authorizeRoles } = require('../config/auth')
 const { getAllNote, addNote, modifyNote, deleteNote } = require('../controller/noteController')
 
-route.get('/', authenticateJWT, authorizeRoles("manager", "employee", "admin"), getAllNote)
-route.post('/', authenticateJWT, authorizeRoles("manager", "employee", "admin"),addNote)
-route.put('/:id', authenticateJWT, authorizeRoles("manager", "employee", "admin"),modifyNote)
-route.delete('/:id',authenticateJWT, authorizeRoles("manager", "employee", "admin"), deleteNote)
+route.get('/', authenticateJWT, authorizeRoles("manager","employee",  "admin"), getAllNote)
+route.post('/', authenticateJWT, authorizeRoles("manager",  "admin"),addNote)
+route.put('/:id', authenticateJWT, authorizeRoles("manager",  "admin"),modifyNote)
+route.delete('/:id',authenticateJWT, authorizeRoles("manager",  "admin"), deleteNote)
 
 module.exports = route
