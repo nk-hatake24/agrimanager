1f5a7ecac710ea580c03dd2f43fc0e60436c6203
Nkenla Michel (mitcode) <nkenlamichel@gmail.com>
2024-06-15 21:53:05 +0200

    user's model  of the chat bot

---
+++ chatbot/src/model/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    id: {
+        type: String,
+        unique: [true , ' the id most be unique']
+       },
+        name_employee:{
+          type: String,
+          required: [true, 'name of the user is required']
+      },
+      function_employee:{
+          type: String,
+          required: [true, 'the function of the user is required'],
+          enum:['employee', 'manager', 'admin']
+      },
+        email: {
+          type: String,
+          required: [true, "email is required"],
+          unique: true
+        },
+        password: {
+          type: String,
+          required: [true, "password is required"],
+        },
+        salary: {
+          type: String,
+        },
+        address: {
+          type: String,
+        },
+        conversationHistory: [{ type: Schema.Types.ObjectId, ref: 'Conversation' }]
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
