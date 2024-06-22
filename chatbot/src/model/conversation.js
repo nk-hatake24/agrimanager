f0522fa8a81969813a91eca7d6ac37ee5b649ece
Nkenla Michel (mitcode) <nkenlamichel@gmail.com>
2024-06-22 12:23:08 +0200

    conversation modele update

--- chatbot/src/model/conversation.js
+++ chatbot/src/model/conversation.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const conversationSchema = new Schema({
-    conversationID: { type: String, required: true, unique: true },
-    userID: { type: String, required: true, unique: true },
+    userID: { type: Schema.Types.ObjectId, ref:'User'},
     messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }]
   });
   
