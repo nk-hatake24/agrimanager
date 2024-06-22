8d09c88edf8ddcfc37e0d88bb297362ef02a2d5a
Nkenla Michel (mitcode) <nkenlamichel@gmail.com>
2024-06-22 13:18:53 +0200

    message get and response get route

--- chatbot/src/route/messageRoute.js
+++ chatbot/src/route/messageRoute.js
@@ -1,6 +1,9 @@
 const route = require('express').Router()
-const {sendMessage} = require('../controller/messageController')
+const {sendMessage, getMessages, getResponse} = require('../controller/messageController')
 
 route.post('/', sendMessage)
+route.get('/message', getMessages)
+route.get('/response', getResponse)
+
 
 module.exports = route
