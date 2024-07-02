9e4c15eb48d046264eaa71892f5ce3a0064f8c4e
Nkenla Michel (mitcode) <nkenlamichel@gmail.com>
2024-07-02 14:37:03 +0200

    Ras

--- chatbot/src/model/response.js
+++ chatbot/src/model/response.js
@@ -5,7 +5,7 @@ const responseSchema = new Schema({
     // responseID: { type: String, required: true, unique: true },
     messageID: { type: Schema.Types.ObjectId, ref: 'Message' },
     timestamp: { type: Date, default: Date.now },
-    content: { type: String, required: true }
+    content: { type: String, required: true },
   });
   
   const Response = mongoose.model('Response', responseSchema);
